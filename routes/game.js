067c99aa32c7fe75275b4a1a3077ea875eadc2d7
MrDomoArigato98 <domdob98@gmail.com>
2025-07-25 12:48:25 +0100

    Finished

--- routes/game.js
+++ routes/game.js
@@ -8,45 +8,15 @@ ROOT is /games
 */
 router.get("/", gameController.getAllGames); //List all games
 
-// router.get("/new", gameController.addNewGameGetForm);
+
+// router.get("/:id", gameController.getGameById); //Get specific Id
+// //How do we edit the game the user is clicked on?
+// router.get("/:id/edit", gameController.editGameGetForm);
 // router.post(
-//   "/new",
-//   [
-//     body("gameTitle")
-//       .trim()
-//       .escape()
-//       .isLength({ min: 1, max: 100 })
-//       .withMessage("Title must be 1-100 characters"),
-
-//     body("publisher")
-//       .trim()
-//       .escape()
-//       .isLength({ min: 1, max: 100 })
-//       .withMessage("Publisher must be 1-100 characters"),
-
-//     body("genre")
-//       .trim()
-//       .escape()
-//       .isLength({ min: 1, max: 100 })
-//       .withMessage("Publisher must be 1-100 characters"),
-
-//     body("releaseYear")
-//       .trim()
-//       .escape()
-//       .isInt({ min: 1970, max: new Date().getFullYear() + 1 })
-//       .withMessage("Enter a valid four digit year."),
-//   ],
-//   gameController.addNewGamePost
+//   "/:id/edit",
+//   gameController.editGamePost
 // );
 
-router.get("/:id", gameController.getGameById); //Get specific Id
-//How do we edit the game the user is clicked on?
-router.get("/:id/edit", gameController.editGameGetForm);
-router.post(
-  "/:id/edit",
-  gameController.editGamePost
-);
-
-router.post("/:gameId/delete", gameController.deleteGamePost);
+// router.post("/:gameId/delete", gameController.deleteGamePost);
 
 export default router;
